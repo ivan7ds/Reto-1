b1c9d6eefaedf4bd750eccbcad1a947a88c0acca
ivan7work <ivan7work@gmail.com>
2021-03-06 12:01:02 +0100

    v.0.3
    cambiadas variables opt a brand

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-var opt_1 = new Array ("-","DUT","UTQ");
-var opt_2 = new Array ("-", "OYS","BSK");
-var opt_3 = new Array ("-", "STR","ZHO");
-var opt_4 = new Array ("-", "LFT","PUL");
+var brand_1 = new Array ("-","DUT","UTQ");
+var brand_2 = new Array ("-", "OYS","BSK");
+var brand_3 = new Array ("-", "STR","ZHO");
+var brand_4 = new Array ("-", "LFT","PUL");
         
 function itx_split(){
     var split;
@@ -10,23 +10,23 @@ function itx_split(){
     //se chequea si la "split" esta definida
     if(split!=0){
     //selecionamos las splits Correctas
-        mis_opts=eval("opt_" + split);
+        mis_brands=eval("brand_" + split);
         //se calcula el numero de splits
-        num_opts=mis_opts.length;
-        //marco el numero de opt en el select
-        document.formulario1.opt.length = num_opts;
-        //para cada opt del array, la pongo en el select
-        for(i=0; i<num_opts; i++){
-             document.formulario1.opt.options[i].value=mis_opts[i];
-            document.formulario1.opt.options[i].text=mis_opts[i];
+        num_brands=mis_brands.length;
+        //marco el numero de brand en el select
+        document.formulario1.brand.length = num_brands;
+        //para cada brand del array, la pongo en el select
+        for(i=0; i<num_brands; i++){
+             document.formulario1.brand.options[i].value=mis_brands[i];
+            document.formulario1.brand.options[i].text=mis_brands[i];
         }
     }else{
-    //si no habia ninguna opt seleccionada, elimino las splits del select
-        document.formulario1.opt.length = 1;
-        //ponemos un guion en la unica opt que he dejado
-        document.formulario1.opt.options[0].value="-";
-        document.formulario1.opt.options[0].text="-";
+    //si no habia ninguna brand seleccionada, elimino las splits del select
+        document.formulario1.brand.length = 1;
+        //ponemos un guion en la unica brand que he dejado
+        document.formulario1.brand.options[0].value="-";
+        document.formulario1.brand.options[0].text="-";
     }
-    //hacer un reset de las opts
-    document.formulario1.opt.options[0].selected = true;     
+    //hacer un reset de las brands
+    document.formulario1.brand.options[0].selected = true;     
 }
